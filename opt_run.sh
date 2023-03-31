d829a04e49bcb83a0779c644eec61d9105cba06d
giaabaoo <dinhhogiabao@gmail.com>
2023-03-31 05:20:37 +0000

    update .gitignore

--- opt_run.sh
+++
@@ -1,7 +0,0 @@
-python opt_chatgpt_dial_gen.py \
---api_key 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 \
---syn_dials synCP_dials.json \
---language Mandarin \
---num_samples 200
-# python process_chatgpt_results.py
-# python gen_annotations.py
